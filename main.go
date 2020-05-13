6ef22a1006ae527b24f389339b7924c07c304eff
Nick Dumas <dumasn@homes.com>
2020-05-12 21:02:13 -0400

    Adding a ticks flag
    Time to start treating this like a proper application.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/EngoEngine/ecs"
 	// "log"
 	// "time"
@@ -10,19 +12,37 @@ func main() {
 	var w ecs.World
 	rs := &RenderSystem{}
 	hs := &HungerSystem{}
+	ds := &DecisionSystem{}
 	w.AddSystem(rs)
 	w.AddSystem(hs)
+	w.AddSystem(ds)
 
 	bob := Creep{
 		BasicEntity:       ecs.NewBasic(),
-		HungerComponent:   HungerComponent{Hunger: 5},
+		HungerComponent:   HungerComponent{Hunger: 5, HungerFactor: 2},
 		IdentityComponent: IdentityComponent{Name: "Bob"},
 	}
 	rs.Add(&bob)
 	hs.Add(&bob)
+	ds.Add(&bob)
 
+	john := Creep{
+		BasicEntity:       ecs.NewBasic(),
+		HungerComponent:   HungerComponent{Hunger: 5, HungerFactor: 3},
+		IdentityComponent: IdentityComponent{Name: "John"},
+	}
+	rs.Add(&john)
+	hs.Add(&john)
+	ds.Add(&john)
+
+	var (
+		ticks int
+	)
+
+	flag.IntVar(&ticks, "ticks", 10, "number of ticks to simulate")
+	flag.Parse()
 	// run the world for 10 seconds
-	for i := 0; i < 10; i++ {
+	for i := 0; i < ticks; i++ {
 		w.Update(1)
 	}
 
