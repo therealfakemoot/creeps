e26a1f06913876485b08795e400f6368616ebfc5
Nick Dumas <dumasn@homes.com>
2020-05-12 21:00:39 -0400

    Add a Hunger scaling factor.
    The formula can be more complex but for now this will allow you to have
    actors that get hungry at different rates.

--- hunger.go
+++ hunger.go
@@ -4,15 +4,18 @@ import ()
 
 // HungerComponent tracks how hungry an entity is.
 type HungerComponent struct {
-	Hunger int
+	Hunger       int
+	HungerFactor int
 }
 
+// HungerSystem handles actions based on hunger.
 type HungerSystem struct {
 	BaseSystem
 }
 
+// Update will increment hunger for all entitites
 func (hs *HungerSystem) Update(dt float32) {
 	for i := 0; i <= len(hs.entities)-1; i++ {
-		hs.entities[i].Hunger++
+		hs.entities[i].Hunger += hs.entities[i].HungerFactor
 	}
 }
